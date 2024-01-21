847fad3ad97e71c91a2ecc668fecb06d1a401636
krgau020 <krgau020@gmail.com>
2024-01-21 13:04:40 +0530

    build model

---
+++ src/utils.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import pickle
+import numpy as np 
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
+
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.metrics import r2_score
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models, param):
+    try:
+        report = {}
+        for i in range(len(models)):
+
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            rs = RandomizedSearchCV(model, para , cv=3 )
+            rs.fit(X_train, y_train)
+
+            model.set_param(**rs.best_params_)
+            model.fit(X_train, y_train)
+
+
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+            y_train_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train,y_train_pred)
+
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+    
+    except Exception as e:
+            logging.info('Exception occured during model training')
+            raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
